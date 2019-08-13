ab7c09e3870b12ea82b58fa5c254c6baf27e624b
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-08-12 22:36:44 -0300

    Moved files served through HTTPS to be served locally

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(ldap.css)
+
+# Update assets pipeline path wiwth fonts
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
