7e6af2e9a6eb73d330df22a1d179ff575f53e0d8
Felipe Cecagno <fcecagno@gmail.com>
2020-01-13 16:25:55 -0300

    customize for Scriba

---
+++ public/hotsite/js/video.js
@@ -0,0 +1,28 @@
+/* -----------------------------------------------------------------------------
+
+Alterado em setembro 2019 por Lourilúcio Moura
+
+-------------------------------------------------------------------------------- */
+
+function lightbox_open() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  document.getElementById('light').style.display = 'block';
+  document.getElementById('fade').style.display = 'block';
+  lightBoxVideo.play();
+}
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  document.getElementById('light').style.display = 'none';
+  document.getElementById('fade').style.display = 'none';
+  lightBoxVideo.pause();
+}
+
+window.document.onkeydown = function(e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
+  }
+}
